82b1375ae7cd342b8a28be58830f2cdc01acc08b
angelTormos <148451184+angelTormos@users.noreply.github.com>
2026-01-09 13:43:03 +0100

    Add files via upload

---
+++ Practica 2/Ejercicio3.ps1
@@ -0,0 +1,6 @@
+﻿[int] $horas = Read-Host "Introduce el numero de horas trabajadas"
+[int] $precio = Read-Host "Introduce el precio por hora"
+
+$salario = $horas * $precio
+
+Write-Host "Tu salario es de $salario €"

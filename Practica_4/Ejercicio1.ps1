31a4327522942b33bbc7f8466f22d2bb1a2083b7
angelTormos <148451184+angelTormos@users.noreply.github.com>
2026-01-16 13:33:01 +0100

    Add files via upload

---
+++ Practica_4/Ejercicio1.ps1
@@ -0,0 +1,6 @@
+﻿$nombre = Read-Host "Dime tu nombre"
+[int]$numero = Read-Host "Dame un numero entero"
+
+for ($a = 0; $a -lt $numero; $a++){
+    Write-Host $nombre
+}
---
+++ Practica_4/Ejercicio3.ps1
@@ -0,0 +1,9 @@
+﻿[int]$num = Read-Host "Introduce un numero entero"
+
+for ($a = $num; $a -ge 0; $a--){
+    if ($a -ne 0){
+        Write-Host -NoNewline "$a, "
+    }else{
+        Write-Host $a
+    }
+}

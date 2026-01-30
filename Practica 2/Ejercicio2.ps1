815df9ed2ff460de4556607d9138ff246879080d
angelTormos <148451184+angelTormos@users.noreply.github.com>
2026-01-30 12:27:21 +0100

    Delete Practica 2 directory

--- Practica 2/Ejercicio2.ps1
+++
@@ -1,9 +0,0 @@
-﻿[int] $num1 = Read-Host "Introduce el primer numero"
-[int] $num2 = Read-Host "Introduce el segundo numero"
-
-$sum = $num1 + $num2
-$res = $num1 - $num2
-$mult = $num1 * $num2
-$div = $num1 / $num2
-
-Write-Host "Suma: $sum, Resta: $res, Multiplicacion: $munlt, Division: $div"

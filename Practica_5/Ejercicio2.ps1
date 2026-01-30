a14e511a85ee637ccf660aeb0bf48565cf189a98
angelTormos <148451184+angelTormos@users.noreply.github.com>
2026-01-30 12:26:57 +0100

    Delete Practica_5 directory

--- Practica_5/Ejercicio2.ps1
+++
@@ -1,5 +0,0 @@
-﻿$usuarios = Import-Csv -Path ".\usuarios.csv" -Delimiter ","
-
-foreach ($usuario in $usuarios) {
-    Write-Host "Nombre: $($usuario.nombre) Apellidos: $($usuario.apellidos) Grupo: $($usuario.grupo)"
-}

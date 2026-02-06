762588ce20d3534e20868fffff05e09ad0860455
angelTormos <148451184+angelTormos@users.noreply.github.com>
2026-02-06 13:14:08 +0100

    Add files via upload

---
+++ UT7/UT7_Practica4.ps1
@@ -0,0 +1,6 @@
+﻿Import-Csv "C:\Users\Administrador\Downloads\archivos\empleados.csv" -delimiter ";" | ForEach-Object {
+    $nombre = "$($_.nombre).$($_.apellido)"
+    if ($_.departamento -eq "PERSONAL"){
+        Set-ADUser -Identity $nombre -ProfilePath \\WIN-7B6PU35SQJI\Empresa_users$\$nombre\
+    }
+}
